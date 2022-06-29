79c99d8a3cfdf20462238d386087b0b419b710eb
Masha Rodionova <rodionovamv@gmai.com>
2022-06-29 21:11:07 +0300

    Add repository, change tests, add Mockito

--- src/main/java/AfishaItems.java
+++ src/main/java/AfishaItems.java
@@ -2,10 +2,13 @@ public class AfishaItems {
     private String name;
     private String genre;
 
+
     public AfishaItems(String name, String genre) {
         this.name = name;
         this.genre = genre;
     }
 
-
+    public String getName(){
+        return name;
+    }
 }
--- src/test/java/ManagerAfishaTest.java
+++ src/test/java/ManagerAfishaTest.java
@@ -1,8 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 
 
 public class ManagerAfishaTest {
+    AfishaRepository repo = Mockito.mock(AfishaRepository.class);
+    ManagerAfisha manager = new ManagerAfisha(repo);
 
     AfishaItems item1 = new AfishaItems("Breaking Bad", "Serial");
     AfishaItems item2 = new AfishaItems("The Bridges of Madison County", "Melodrama");
@@ -18,108 +23,26 @@ public class ManagerAfishaTest {
     AfishaItems item12 = new AfishaItems("Shrek", "Cartoon");
     AfishaItems item13 = new AfishaItems("Sex Education", "Serial");
 
-
-    //Тестируем добавление новых фильмов в афишу
     @Test
-    public void test() {
-        ManagerAfisha afisha = new ManagerAfisha();
-        afisha.add(item7);
-        AfishaItems[] expected = {item7};
-        AfishaItems[] actual = afisha.getItems();
+    public void test1() {
+        AfishaItems[] items = {item9, item8, item10, item1, item4, item2, item5, item3, item12, item13};
+        doReturn(items).when(repo).getItems();
+
+        AfishaItems[] expected = {item13, item12, item3, item5, item2, item4, item1};
+        AfishaItems[] actual = manager.findLast(7);
+
         Assertions.assertArrayEquals(expected, actual);
     }
 
-
-    //Тестируем вывод всех фильмов афиши в обратном от добавления порядке
     @Test
     public void test2() {
-        ManagerAfisha afisha = new ManagerAfisha();
-        afisha.add(item1);
-        afisha.add(item11);
-        afisha.add(item13);
-        afisha.add(item7);
-        afisha.add(item2);
-        AfishaItems[] expected = {item2, item7, item13, item11, item1};
-        AfishaItems[] actual = afisha.findAll();
+        AfishaItems[] items = {item9, item8, item10};
+        doReturn(items).when(repo).findAll();
+
+        AfishaItems[] expected = {item9, item8, item10};
+        AfishaItems[] actual = manager.managerFindAll();
+
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
-    public void test3() {
-        ManagerAfisha afisha = new ManagerAfisha();
-        AfishaItems[] expected = {};
-        AfishaItems[] actual = afisha.findAll();
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    // Тестируем вывод запрашиваемого количества (по умолчанию 10) последних
-    // добавленных фильмов в обратном от добавления порядке
-
-    @Test
-    public void test4() {
-        ManagerAfisha afisha = new ManagerAfisha();
-        afisha.add(item1);
-        afisha.add(item11);
-        afisha.add(item13);
-        afisha.add(item7);
-        afisha.add(item2);
-        AfishaItems[] expected = {item2, item7, item13, item11, item1};
-        AfishaItems[] actual = afisha.findLast();
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void test5() {
-        ManagerAfisha afisha = new ManagerAfisha(7);
-        afisha.add(item1);
-        afisha.add(item11);
-        afisha.add(item13);
-        afisha.add(item7);
-        afisha.add(item2);
-        AfishaItems[] expected = {item2, item7, item13, item11, item1};
-        AfishaItems[] actual = afisha.findLast();
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void test6() {
-        ManagerAfisha afisha = new ManagerAfisha();
-        afisha.add(item1);
-        afisha.add(item2);
-        afisha.add(item3);
-        afisha.add(item4);
-        afisha.add(item5);
-        afisha.add(item6);
-        afisha.add(item7);
-        afisha.add(item8);
-        afisha.add(item9);
-        afisha.add(item10);
-        afisha.add(item11);
-        afisha.add(item12);
-        afisha.add(item13);
-        AfishaItems[] expected = {item13, item12, item11, item10, item9, item8, item7, item6, item5, item4};
-        AfishaItems[] actual = afisha.findLast();
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void test7() {
-        ManagerAfisha afisha = new ManagerAfisha(7);
-        afisha.add(item1);
-        afisha.add(item2);
-        afisha.add(item3);
-        afisha.add(item4);
-        afisha.add(item5);
-        afisha.add(item6);
-        afisha.add(item7);
-        afisha.add(item8);
-        afisha.add(item9);
-        afisha.add(item10);
-        afisha.add(item11);
-        afisha.add(item12);
-        afisha.add(item13);
-        AfishaItems[] expected = {item13, item12, item11, item10, item9, item8, item7};
-        AfishaItems[] actual = afisha.findLast();
-        Assertions.assertArrayEquals(expected, actual);
-    }
 }
